c17599ef57318930ed3787a7103c8a80694d1d8e
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-03-15 09:23:54 +0700

    Connect to Database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(() => {
+  console.log("Contected to mongoBD");
+});
 
 const app = express();
 
